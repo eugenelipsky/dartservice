ba9e56a589d7b3995304d95eb7ebbc79d0d6a61e
eugenelipsky <zvlipskiy@mail.ru>
2020-03-07 21:56:49 +0200

    Benefits, numbers, services, reviews sections added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
-$(function(){
+$(function () {
+    $('.toggle').click(function(e) {
+        e.preventDefault();
 
+        var $this = $(this);
+
+        if ($this.next().hasClass('show')) {
+            $this.next().removeClass('show');
+            $this.next().slideUp(350);
+        } else {
+            $this.parent().parent().find('li .inner').removeClass('show');
+            $this.parent().parent().find('li .inner').slideUp(350);
+            $this.next().toggleClass('show');
+            $this.next().slideToggle(350);
+        }
+    });
+
+    $('.reviews__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+    });
 });
